14541a4bdffce3a988412902bcdab156e740eed4
daviddiazh <daviddiazlds1221@gmail.com>
2024-08-05 11:23:31 -0500

    change

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,4 @@
 export const user = import.meta.env.VITE_API_USER;
 export const password = import.meta.env.VITE_API_PASSWORD;
-export const baseURL = import.meta.env.VITE_API_BASE_URL;
+// export const baseURL = import.meta.env.VITE_API_BASE_URL;
+export const baseURL = 'https://api-arrendamientos-alvarez.vercel.app';
